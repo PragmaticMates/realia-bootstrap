06f9392b521be0842b176cb4d6e8bec216d6bfa6
Lukas <lukas.vinclav@gmail.com>
2015-07-13 14:36:24 +0200

    functions.php cleanup

--- functions.php
+++ functions.php
@@ -8,9 +8,10 @@ define( 'AGENCY_EXCERPT_LENGTH', 20 );
 
 /**
  * Custom excerpt length
+ *
+ * @param $length
+ * @return int
  */
-add_filter('excerpt_length', 'realia_bootstrap_excerpt_length' );
-
 function realia_bootstrap_excerpt_length( $length ) {
 	global $post;
 
@@ -22,12 +23,13 @@ function realia_bootstrap_excerpt_length( $length ) {
 
 	return $length;
 }
+add_filter('excerpt_length', 'realia_bootstrap_excerpt_length' );
 
 /**
  * Enqueue scripts & styles
+ *
+ * @return void
  */
-add_action( 'wp_enqueue_scripts', 'realia_bootstrap_enqueue_files' );
-
 function realia_bootstrap_enqueue_files() {
 	wp_register_script( 'bootstrap-collapse', get_template_directory_uri() . '/assets/libraries/bootstrap/javascripts/bootstrap/collapse.js' );
 	wp_enqueue_script( 'bootstrap-collapse' );
@@ -35,12 +37,13 @@ function realia_bootstrap_enqueue_files() {
 	wp_register_style( 'realia-bootstrap', get_template_directory_uri() . '/assets/css/realia-bootstrap.css' );
 	wp_enqueue_style( 'realia-bootstrap' );
 }
+add_action( 'wp_enqueue_scripts', 'realia_bootstrap_enqueue_files' );
 
 /**
  * Additional after theme setup functions
+ *
+ * @return void
  */
-add_action( 'after_setup_theme', 'realia_bootstrap_after_theme_setup' );
-
 function realia_bootstrap_after_theme_setup() {
 	add_theme_support( 'realia-custom-styles' );
 	add_theme_support( 'automatic-feed-links' );
@@ -54,21 +57,23 @@ function realia_bootstrap_after_theme_setup() {
 		$content_width = 1170;
 	}
 }
+add_action( 'after_setup_theme', 'realia_bootstrap_after_theme_setup' );
 
 /**
- * Register navigations
+ * Register menus
+ *
+ * @return void
  */
-add_action( 'init', 'realia_bootstrap_menus' );
-
 function realia_bootstrap_menus() {
 	register_nav_menu( 'primary', __( 'Primary', 'realia-bootstrap' ) );
 }
+add_action( 'init', 'realia_bootstrap_menus' );
 
 /**
  * Custom widget areas
+ *
+ * @return void
  */
-add_action( 'widgets_init', 'realia_bootstrap_sidebars' );
-
 function realia_bootstrap_sidebars() {
 	register_sidebar( array( 'name' => __( 'Primary', 'realia-bootstrap' ), 'id' => 'sidebar-1', 'before_widget' => '<div id="%1$s" class="widget %2$s">', 'after_widget' => '</div>' ) );
 	register_sidebar( array( 'name' => __( 'Top Fullwidth', 'realia-bootstrap' ), 'id' => 'sidebar-top-fullwidth', 'before_widget' => '<div id="%1$s" class="widget %2$s">', 'after_widget' => '</div>' ) );
@@ -83,52 +88,56 @@ function realia_bootstrap_sidebars() {
 	register_sidebar( array( 'name' => __( 'Footer Bottom Left', 'realia-bootstrap' ), 'id' => 'footer-bottom-left', 'before_widget' => '<div id="%1$s" class="widget %2$s">', 'after_widget' => '</div>' ) );
 	register_sidebar( array( 'name' => __( 'Footer Bottom Right', 'realia-bootstrap' ), 'id' => 'footer-bottom-right', 'before_widget' => '<div id="%1$s" class="widget %2$s">', 'after_widget' => '</div>' ) );
 }
+add_action( 'widgets_init', 'realia_bootstrap_sidebars' );
 
 /**
  * Disable admin's bar top margin
+ *
+ * @return void
  */
-add_action( 'get_header', 'realia_bootstrap_disable_admin_bar_top_margin' );
-
 function realia_bootstrap_disable_admin_bar_top_margin() {
 	remove_action( 'wp_head', '_admin_bar_bump_cb' );
 }
+add_action( 'get_header', 'realia_bootstrap_disable_admin_bar_top_margin' );
 
 /**
  * Read more for post excerpt
+ *
+ * @param $more
+ * @return void|string
  */
-add_filter( 'excerpt_more', 'realia_bootstrap_excerpt_read_more' );
-
 function realia_bootstrap_excerpt_read_more( $more ) {
 	global $post;
 
 	if ( $post->post_type == 'property' || $post->post_type = 'agency' ) {
-		return;
+		return null;
 	}
 
 	return '<a class="post-read-more" href="'. get_permalink( $post->ID ) . '">' . __( 'Read more', 'megareal' ) . '</a>';
 }
+add_filter( 'excerpt_more', 'realia_bootstrap_excerpt_read_more' );
 
-add_action( 'init', 'realia_bootstrap_register_session' );
-
+/**
+ * Registers sessions
+ *
+ * @return void
+ */
 function realia_bootstrap_register_session(){
 	if( ! session_id() ) {
 		session_start();
 	}
 }
-
+add_action( 'init', 'realia_bootstrap_register_session' );
 
 /**
  * Customizations
+ *
+ * @param $wp_customize
+ * @return void
  */
-add_action( 'customize_register', 'realia_bootstrap_customizations' );
-
 function realia_bootstrap_customizations( $wp_customize ) {
-	/**
-	 * General
-	 */
 	$wp_customize->add_section( 'realia_bootstrap_general', array( 'title' => __( 'Realia Bootstrap General', 'realia-bootstrap' ), 'priority' => 0 ) );
 
-	// Logo
 	$wp_customize->add_setting( 'realia_bootstrap_general_logo', array( 'sanitize_callback' => 'sanitize_text_field' ) );
 	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'header_logo', array(
 		'label'         => __( 'Logo', 'realia-bootstrap' ),
@@ -137,8 +146,20 @@ function realia_bootstrap_customizations( $wp_customize ) {
 		'description'   => __( 'Logo displayed in header. By default it has some opacity added by CSS which will change after hover.', 'realia-bootstrap' ),
 	) ) );
 }
+add_action( 'customize_register', 'realia_bootstrap_customizations' );
 
+/**
+ * Class Realia_Bootstrap_Menu_Walker
+ */
 class Realia_Bootstrap_Menu_Walker extends Walker_Nav_Menu {
+	/**
+	 * Custom parent menu item class
+	 *
+	 * @param string $output
+	 * @param int $depth
+	 * @param array $args
+	 * @return void
+	 */
 	public function start_lvl( &$output, $depth = 0, $args = array() ) {
 		$indent = str_repeat("\t", $depth);
 		$output .= "\n$indent<ul class=\"dropdown-menu sub-menu\">\n";
